4e1187cc797d2752f4cde1684e39f8b891421d1d
arturfreiitas <96545383+arturfreiitas@users.noreply.github.com>
2022-06-13 18:01:09 -0300

    Create ArtBiblioteca.c

---
+++ ArtBiblioteca.c
@@ -0,0 +1,29 @@
+#include "ArtBiblioteca.h"
+#include "stdio.h"
+#include "string.h"
+
+
+int lerMsg(artPonteiro *tp)
+{
+    printf("Coloque a mensagem, char inicial e char final:\n");
+    scanf("%s %c %c", tp->msg_depois, &tp->inicial, &tp->final);
+
+    strncpy(tp->msg_antes, tp->msg_depois, MAX_SIZE);
+
+    return 0;
+}
+
+
+
+{
+    int cont = 0;
+
+    for (int i = 0; tp->msg_depois[i]; ++i) {
+        if (tp->msg_depois[i] == tp->inicial)
+        {
+            tp->msg_depois[i] = tp->final;
+            cont++;
+        }
+    }
+    return cont;
+}
