a41ab9136e5116fbd788719cdd1290378d9a2b2d
arturfreiitas <96545383+arturfreiitas@users.noreply.github.com>
2022-06-13 18:00:10 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "ArtBiblioteca.h"
+#include <stdio.h>
+
+int main(void)
+{
+    //declarando as variaveis
+    int cont;
+    artPonteiro pntr;
+
+    //chama a função para ler as entradas do usuario
+    lerMsg(&pntr);
+
+    //chama a função que troca os caracteres e retorna o contador
+    cont = trocaCar(&pntr);
+
+    //mostrar resultado
+    printf("\n%s\n%s\n%d\n", pntr.msg_antes , pntr.msg_depois, cont);
+
+    return 0;
+}
